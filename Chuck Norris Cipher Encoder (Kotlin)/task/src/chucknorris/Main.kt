cc24ccf6b87f679daa2de85fb7d2304937d4ad7f
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-10-27 22:57:52 +0100

    stage 2

--- Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
@@ -4,7 +4,10 @@ fun main() {
     println("Input string:")
     val input = readln()
 
-    val encoded = input.map { "$it" }.joinToString(" ")
+    val encoded = input.map { it to Integer.toBinaryString(it.code) }.toList()
     println()
-    println(encoded)
+    println("The result:")
+    encoded.forEach { println("${it.first} = ${leadBinString(it.second)}") }
 }
+
+fun leadBinString(binStr: String) = "0000000$binStr".substring(binStr.length)
