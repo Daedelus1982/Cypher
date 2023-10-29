5f942fce34eba8433a11bf23a71f4e6195052fea
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-10-29 14:17:16 +0000

    stage 4

--- Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
@@ -3,11 +3,32 @@ package chucknorris
 fun main() {
     println("Input string:")
     val input = readln()
-
-    val encoded = input.map { it to Integer.toBinaryString(it.code) }.toList()
     println()
     println("The result:")
-    encoded.forEach { println("${it.first} = ${leadBinString(it.second)}") }
+    println(binStrToString(chuckStrToBinStr(input)))
 }
 
-fun leadBinString(binStr: String) = "0000000$binStr".substring(binStr.length)
+fun binStrToChuckStr(binStr: String): String {
+    return binStr.split(Regex("(?<=(.))(?!\\1)"))
+        .filter { it.isNotBlank() }.joinToString(" ") {
+            if (it.first() == '1') "0 ${"0".repeat(it.length)}"
+            else "00 ${"0".repeat(it.length)}"
+        }
+}
+
+fun chuckStrToBinStr(chuckStr: String): String {
+    return "0{1,2}\\s0+".toRegex().findAll(chuckStr)
+        .map { it.value }
+        .map { val pair = it.split(" "); pair.first() to pair.last() }
+        .map {
+            if (it.first.length == 1) "1".repeat(it.second.length)
+            else "0".repeat(it.second.length)
+        }.joinToString("")
+}
+
+
+fun padBinStr(binStr: String) = "0000000$binStr".substring(binStr.length)
+
+fun binStrToString(binStr: String) = binStr.chunked(7).map { it.toInt(2).toChar() }.joinToString("")
+
+fun stringToBinStr(input: String) = input.map { padBinStr(Integer.toBinaryString(it.code)) }.joinToString("")
